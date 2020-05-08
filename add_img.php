160d397828824f962cd26019fe5d42463d685dd5
abigailkwan <60206977+abigailkwan@users.noreply.github.com>
2020-05-08 14:31:46 -0700

    initial commit

---
+++ add_img.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "id12556185_bluejay";
+$password = "seniordesign";
+$database = "id12556185_foodspy"; 
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$foodid = json_decode($_POST["food_id"]);
+$targetDir = "img/";
+$fileName = json_decode($_POST["file_name"]);
+$targetFilePath = $targetDir . $fileName;
+$cameraImage = "https://abigailkwan.000webhostapp.com/img/abby.jpg";
+
+if (!copy($cameraImage, $targetFilePath)){
+    echo "failed to copy";
+}
+
+$sql = "UPDATE FoodProfile SET food_image = '$fileName' WHERE food_id = '$foodid'";
+if(mysqli_query($conn,$sql)){
+    $statusMsg = "Added Picture successfully";
+}else {
+    $statusMsg = "Error inserting".mysqli_error($conn);
+}
+
+
+$conn->close();
+?>
